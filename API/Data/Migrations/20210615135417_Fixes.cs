ca91d08df39429252bd7f59ee5da0cd83e54d734
DanaSarghe <dsarghe@gmail.com>
2021-06-16 11:42:58 +0300

    autoMapper

---
+++ API/Data/Migrations/20210615135417_Fixes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class Fixes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Grnder",
+                table: "Users",
+                newName: "Gender");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "Users",
+                newName: "Grnder");
+        }
+    }
+}
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -24,12 +24,23 @@ namespace API.Services
             };
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var nowUtc = DateTimeOffset.UtcNow;
+            var expiration = nowUtc.AddMinutes(100);
+            var exp = expiration.ToUnixTimeSeconds();
+            var now = nowUtc.ToUnixTimeSeconds();
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
+                //Expires = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+                Expires = DateTime.UtcNow.AddDays(7),
+                NotBefore = DateTime.Now.Subtract(TimeSpan.FromMinutes(30)),
                 SigningCredentials = creds
+                // var nowUtc = DateTimeOffset.UtcNow;
+                // var expiration = nowUtc.AddMinutes(jwtOptions.ExpiryMinutes);
+                // var exp = expiration.ToUnixTimeSeconds();
+                // var now = nowUtc.ToUnixTimeSeconds();
+                // DateTime.Now.AddDays(7)
             };
 
             var tokenHandler = new JwtSecurityTokenHandler();
