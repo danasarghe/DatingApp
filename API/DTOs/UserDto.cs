bc078811ce62dbb85a7a592649a3e63a92e503ba
DanaSarghe <dsarghe@gmail.com>
2021-06-25 10:51:13 +0300

    photo_upload_delete_setMain

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -3,6 +3,9 @@ namespace API.DTOs
     public class UserDto
     {
         public string UserName { get; set; }
-        public string Token { get; set; }
+        public string Token { get; set; } 
+        public string PhotoUrl { get; set; } 
+        //public string KnownAs { get; set; } 
+        //public string Gender { get; set; } 
     }
 }
